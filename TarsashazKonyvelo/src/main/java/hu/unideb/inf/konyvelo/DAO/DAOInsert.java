fb122ed538a850a0daa99a2e81f446cc30cb0188
Bence Szabó <bence.szabo944@gmail.com>
2015-05-08 21:07:30 +0200

    Fájlok lemaradtak

---
+++ TarsashazKonyvelo/src/main/java/hu/unideb/inf/konyvelo/DAO/DAOInsert.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package hu.unideb.inf.konyvelo.DAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import hu.unideb.inf.konyvelo.Model.Lakas;
+import hu.unideb.inf.konyvelo.Model.Tarsashaz;
+import hu.unideb.inf.konyvelo.Model.TranzakcioL;
+import hu.unideb.inf.konyvelo.Model.TranzakcioT;
+
+/**
+ * @author Bence
+ *
+ */
+public class DAOInsert {
+
+	public void insertTarsashaz(Tarsashaz tarsashaz) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("INSERT INTO TARSASHAZ (TARSASHAZID, CIM, TARTOZAS) VALUES(?,?,?)");
+			preparedStatement.setInt(1, tarsashaz.getId());
+			preparedStatement.setString(2, tarsashaz.getCim());
+			preparedStatement.setInt(3, tarsashaz.getTartozas());
+			
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void insertLakas(Lakas lakas) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("INSERT INTO LAKAS (LAKASID, TULAJDONOS, TARSASHAZID, EMELET, AJTO, TARTOZAS)"
+							+ " VALUES(?,?,?,?,?,?)");
+			preparedStatement.setInt(1, lakas.getId());
+			preparedStatement.setString(2, lakas.getTulajdonos());
+			preparedStatement.setInt(3, lakas.getTarsashazID());
+			preparedStatement.setInt(4, lakas.getEmelet());
+			preparedStatement.setInt(5, lakas.getAjto());
+			preparedStatement.setInt(6, lakas.getTartozas());
+
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void insertTranzakcioL(TranzakcioL tranzakcioL) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("INSERT INTO TRANZAKCIOL (TRANZAKCIOLID, LAKASID, OSSZEG, DATUM, LEIRAS, BEFIZETO) "
+							+ "VALUES(?,?,?,?,?,?)");
+			preparedStatement.setInt(1, tranzakcioL.getTranzakcioLID());
+			preparedStatement.setInt(2, tranzakcioL.getLakasID());
+			preparedStatement.setInt(3, tranzakcioL.getOsszeg());
+			preparedStatement.setDate(4, new Date(tranzakcioL.getDatum()
+					.getMillis()));
+			preparedStatement.setString(5, tranzakcioL.getLeiras());
+			preparedStatement.setString(6, tranzakcioL.getBefizeto());
+
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void insertTranzakcioT(TranzakcioT tranzakcioT) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("INSERT INTO TRANZAKCIOT (TRANZAKCIOTID, TARSASHAZID, OSSZEG, DATUM, LEIRAS, KIIRO) "
+							+ "VALUES(?,?,?,?,?,?)");
+			preparedStatement.setInt(1, tranzakcioT.getTranzakcioTID());
+			preparedStatement.setInt(2, tranzakcioT.getTarsashazID());
+			preparedStatement.setInt(3, tranzakcioT.getOsszeg());
+			preparedStatement.setDate(4, new Date(tranzakcioT.getDatum()
+					.getMillis()));
+			preparedStatement.setString(5, tranzakcioT.getLeiras());
+			preparedStatement.setString(6, tranzakcioT.getKiiro());
+
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ TarsashazKonyvelo/src/main/java/hu/unideb/inf/konyvelo/DAO/DAOUpdate.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package hu.unideb.inf.konyvelo.DAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import hu.unideb.inf.konyvelo.Model.Lakas;
+import hu.unideb.inf.konyvelo.Model.Tarsashaz;
+import hu.unideb.inf.konyvelo.Model.TranzakcioL;
+import hu.unideb.inf.konyvelo.Model.TranzakcioT;
+
+/**
+ * @author Bence
+ *
+ */
+public class DAOUpdate {
+
+	public void updateTarsashaz(Tarsashaz tarsashaz) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("UPDATE TARSASHAZ SET CIM = ?, TARTOZAS = ? WHERE TARSASHAZID = ?");
+			preparedStatement.setString(1, tarsashaz.getCim());
+			preparedStatement.setInt(2, tarsashaz.getTartozas());
+			preparedStatement.setInt(3, tarsashaz.getId());
+
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateLakas(Lakas lakas) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("UPDATE LAKAS SET TULAJDONOS = ?, EMELET = ?, AJTO = ?, TARTOZAS = ?"
+							+ " WHERE LAKASID = ?");
+			preparedStatement.setString(1, lakas.getTulajdonos());
+			preparedStatement.setInt(2, lakas.getEmelet());
+			preparedStatement.setInt(3, lakas.getAjto());
+			preparedStatement.setInt(4, lakas.getTartozas());
+			preparedStatement.setInt(5, lakas.getId());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateTranzakcioL(TranzakcioL tranzakcioL) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("UPDATE TRANZAKCIOL SET LAKASID = ?, OSSZEG = ?, DATUM = ?, LEIRAS = ?, BEFIZETO = ?"
+							+ " WHERE TRANZAKCIOLID = ?");
+			preparedStatement.setInt(1, tranzakcioL.getLakasID());
+			preparedStatement.setInt(2, tranzakcioL.getOsszeg());
+			preparedStatement.setDate(3, new Date(tranzakcioL.getDatum()
+					.getMillis()));
+			preparedStatement.setString(4, tranzakcioL.getLeiras());
+			preparedStatement.setString(5, tranzakcioL.getBefizeto());
+			preparedStatement.setInt(6, tranzakcioL.getTranzakcioLID());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateTranzakcioT(TranzakcioT tranzakcioT) {
+		try (Connection connection = ConnectionFactory.getConnection()) {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("UPDATE TRANZAKCIOT SET TARSASHAZID = ?, OSSZEG = ?, DATUM = ?, LEIRAS = ?, KIIRO = ?"
+							+ " WHERE TRANZAKCIOTID = ?");
+			preparedStatement.setInt(1, tranzakcioT.getTarsashazID());
+			preparedStatement.setInt(2, tranzakcioT.getOsszeg());
+			preparedStatement.setDate(3, new Date(tranzakcioT.getDatum()
+					.getMillis()));
+			preparedStatement.setString(4, tranzakcioT.getLeiras());
+			preparedStatement.setString(5, tranzakcioT.getKiiro());
+			preparedStatement.setInt(6, tranzakcioT.getTranzakcioTID());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
